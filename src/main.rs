48f30ac8c5c4a908439fe7cc26a6ce65195afbd5
Rafael Santos <rafa@blackmagicbox.com.br>
2024-01-07 01:44:33 +0100

    FEAT: Render method

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use std::error::Error;
+use std::thread;
 use std::time::Duration;
 use crossterm::{event, ExecutableCommand, terminal};
 use crossterm::cursor::{Hide, Show};
 use crossterm::event::{Event, KeyCode};
 use crossterm::terminal::{EnterAlternateScreen, LeaveAlternateScreen};
 use rusty_audio::Audio;
+use rust_invaders::{frame, render};
 
 fn main() -> Result<(), Box<dyn Error>>{
     let mut audio = Audio::new();
@@ -22,8 +24,28 @@ fn main() -> Result<(), Box<dyn Error>>{
     stdout.execute(EnterAlternateScreen)?;
     stdout.execute(Hide)?;
 
+    // Render loop in a different thread
+    let (render_tx, render_rx) = std::sync::mpsc::channel();
+    let render_handle = thread::spawn(move || {
+        let mut last_frame = frame::new_frame();
+        let mut stdout = std::io::stdout();
+        render::render(&mut stdout, &last_frame, &last_frame, true);
+        loop {
+            let curr_frame = match render_rx.recv() {
+                Ok(x) => x,
+                Err(_) => break,
+            };
+            render::render(&mut stdout, &last_frame, &curr_frame, false);
+            last_frame = curr_frame;
+        }
+    });
+
+
     // Game Loop
     'gameloop: loop {
+        // Per-frame init
+        let curr_frame = frame::new_frame();
+        // Input
         while event::poll(Duration::default())? {
             if let Event::Key(key_event) = event::read()? {
                 match key_event.code {
@@ -36,9 +58,14 @@ fn main() -> Result<(), Box<dyn Error>>{
             }
 
         }
+        // Draw & render
+        let _ = render_tx.send(curr_frame);
+        thread::sleep(Duration::from_millis(1));
     }
 
-    // Wait for the startup sound to finish playing
+    // Cleanup
+    drop(render_tx);
+    render_handle.join().unwrap();
     audio.wait();
     stdout.execute(Show)?;
     stdout.execute(LeaveAlternateScreen)?;
